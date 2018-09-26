5cd21a91601bb8a7fc559f866e0d59b180058a79
zhuwenge123 <admin@FanydeiMac13.local>
2018-09-26 11:23:32 +0800

    x+y
    x+y

--- p9.26/ViewController.swift
+++ p9.26/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+   
+    @IBOutlet weak var z: UITextField!
+    @IBOutlet weak var y: UITextField!
+    @IBOutlet weak var x: UITextField!
+    @IBAction func dy(_ sender: Any) {
+        z.text="\(Int(x.text!)!+Int(y.text!)!)"
+           }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
